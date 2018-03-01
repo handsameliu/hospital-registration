940ee016bebaee3807cecec347df9b1ef2f9ea58
handsameliu <wei.liu@semioe.com>
2018-03-01 16:41:05 +0800

    增加echarts图表，统计并展示就诊人数

--- routes/page_router.js
+++ routes/page_router.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const pageController = require("../controller/pageController.js");
 
-router.get('/',pageController.signIn); /*首页*/
+router.get('/',pageController.signIn); /*登录*/
 router.get('/index',pageController.index); /*首页*/
 
 module.exports = router;
