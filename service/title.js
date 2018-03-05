e27cbc252a1dad0f0ef4ed5ed3420b1fd7b8df3f
handsameliu <wei.liu@semioe.com>
2018-03-05 15:42:01 +0800

    完善title功能

---
+++ service/title.js
@@ -0,0 +1,92 @@
+'use strict';
+
+let {db} = require('../db');
+let {message} = require('../helper');
+let titleService = db.Title;
+
+/**
+ * 新增科室
+ * @param {name 科室名称，address 科室地址，desc 科室备注} req 
+ * @param {*} res 
+ */
+exports.addTitle = (req, res) => {
+    let body = req.body;
+    if(!body || !(body.name)){
+        return res.json(message('params invalid'));
+    }
+    titleService.findOne({name: body.name, desc: body.desc}).exec((err, data) => {
+        if (err) {
+            return res.json(message(err));
+        }
+        if (data) {
+            return res.json(message('title repeat'));
+        }
+        titleService.create(body, (error, data) => {
+            console.log(error);
+            if(error){
+                return res.json(message('addTitle error', error));
+            }
+            res.json(message(null, {error_code: 0, message: 'SUCCESS'}));
+        })
+    });
+}
+/**
+ * 
+ * @param {_id 被修改的科室id，name 科室名称，address 科室地址，desc 科室备注} req 
+ * @param {*} res 
+ */
+exports.editTitle = (req, res) => {
+    let id = req.body._id;
+    titleService.findByIdAndUpdate(req.body._id,{$set:{name: req.body.name, desc: req.body.desc}}).exec((err, data) => {
+        if (err) {
+            return res.json(message(err));
+        }
+        res.json(message(null, {error_code: 0, message: 'SUCCESS'}));
+    })
+}
+/**
+ * 删除科室id
+ * @param {_id 要删除的科室id} req 
+ * @param {*} res 
+ */
+exports.delTitle = (req, res) => {
+    let id = req.body._id;
+    titleService.remove({_id: id}, (err) => {
+        if (err) {
+            return res.json(message(err));
+        }
+        res.json(message(null, {error_code: 0, message: 'SUCCESS'}));
+    })
+}
+/**
+ * 根据科室id查询科室信息
+ * @param {_id 要查询的科室id} req 
+ * @param {*} res 
+ */
+exports.getTitleById = (req, res) => {
+    console.log(req.params.titleId)
+    titleService.findById(req.params.titleId).exec((err, data) => {
+        if (err) {
+            return res.json(message(err));
+        }
+        res.json(message(null, {error_code: 0, message: 'SUCCESS', result: data}));
+    })
+}
+/**
+ * 通过查询条件查询列表
+ * @param {name 科室名称，address 科室地址，desc 科室备注} req 
+ * @param {*} res 
+ */
+exports.getTitleList = (req, res) => {
+    console.log(req.query)
+    // 以下两个方法都可以查询到对应信息，查询条件必须都要有，否则视为空，会查询到所有
+    // titleService.find({$or: [{name: new RegExp(req.query.name, 'g')}, {address: new RegExp(req.query.address, 'g')}, {desc: new RegExp(req.query.desc, 'g')}]}).exec((err ,data) => {
+    // 使用and并排查询即可，$or会把所有符合条件的都查询出来（如果条件为空，那极有可能会查询出所有）
+    titleService.find({name: {$regex: req.query.name, $options: '$i'}, desc: {$regex: req.query.desc, $options: '$i'}}).exec((err ,data) => {
+        // $options: '$i' 忽略大小写选项
+        if(err){
+            return res.json(message(err));
+        }
+        res.json(message(null,{error_code:0,message:'success',result:data}));
+    });
+}
